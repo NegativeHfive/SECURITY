373efeea69748be5c448023a79f7ba6dad879cbd
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-03-13 11:55:12 +0100

    opdracht 3 af

--- OPDRACHT1/guestbook_getopdracht2.php
+++ OPDRACHT1/guestbook_getopdracht2.php
@@ -1,6 +1,4 @@
 <?php
-
-//session_start();
 $servername = "localhost:3311";
 $username = "root"; // PAS DEZE AAN ALS DAT NODIG IS
 $password = ""; // PAS DEZE AAN ALS DAT NODIG IS
@@ -11,11 +9,6 @@ try {
 } catch (Exception $e) {
     die("Failed to open database connection, did you start it and configure the credentials properly?");
 }
-
-if (empty($_SESSION['token'])) {
-    $_SESSION['token'] = bin2hex(random_bytes(32));
-}
-$token = $_SESSION['token'];
 ?>
 <html>
 <head>
@@ -28,57 +21,37 @@ $token = $_SESSION['token'];
         .body-container {
             background-color: aliceblue;
             width: 200px;
+            height: 100%;
             margin-left: auto;
             margin-right: auto;
             padding-left: 100px;
             padding-right: 100px;
-            padding-bottom: 20px;
         }
 
         .heading {
             text-align: center;
-        }
-
-        .disclosure-notice {
-            color: lightgray;
         }
     </style>
 </head>
 <body>
 <div class="body-container">
     <h1 class="heading">Gastenboek 'De lekkage'</h1>
-    <form action="guestbook.php" method="post">
-        Email: <input type="email" name="email"><br/>
+    <form action="guestbook_get.php">
+        Email: <input type="email" name="email" value=""><br/>
         <input type="hidden" value="red" name="color">
         Bericht: <textarea name="text" minlength="4"></textarea><br/>
         <?php if (userIsAdmin($conn)) {
-            echo "<input type=\"hidden\" name=\"admin\" value=" . $_COOKIE['admin'] . "\">";
+            echo "<input type\"hidden\" name=\"admin\" value=" . $_COOKIE['admin'] . "\">";
         } ?>
-        <input type="hidden" name="token" value="<?php echo $token; ?>">
+        <script>alert('Hallo ik ben XSS');</script>
+        <script>document.body.style.backgroundColor = 'red';</script>
         <input type="submit">
     </form>
     <hr/>
     <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $email = $_POST['email'];
-        $text = htmlspecialchars($_POST['text']);
-        $admin = isset($_POST['admin']) ? 1 : 0;
-        $color = $_POST['color'];
-        
-        if (filter_var($email , FILTER_VALIDATE_EMAIL)){
-            $stmt = $conn->prepare("INSERT INTO `entries` (`email`, `color`, `admin`, `text`) VALUES (:email, :color, :admin, :text)");
-                $stmt->bindParam(':email', $email);
-                $stmt->bindParam(':color', $color);
-                $stmt->bindParam(':admin', $admin);
-                $stmt->bindParam(':text', $text);
-                $stmt->execute();
-                echo "<p>Bericht succesvol toegevoegd!</p>";
-        } else {
-            die('wrong format email address');
-        }
-        
-        
-
+    if (isset($_GET['email']) && isset($_GET['text'])) {
+        print "<div style=\"color: red\">Email: " . $_GET['email'];
+        print ": " . $_GET['text'] . "</div><br/>";
     }
 
 
@@ -109,19 +82,6 @@ $token = $_SESSION['token'];
     }
 
     ?>
-    <hr/>
-    <div class="disclosure-notice">
-        <p>
-            Hierbij krijgt iedereen expliciete toestemming om dit Gastenboek zelf te gebruiken voor welke doeleinden dan
-            ook.
-        </p>
-        <p>
-            Onthoud dat je voor andere websites altijd je aan de princiepes van
-            <a href="https://en.wikipedia.org/wiki/Responsible_disclosure" target="_blank" style="color: lightgray;">
-                Responsible Disclosure
-            </a> wilt houden.
-        </p>
-    </div>
 </div>
 </body>
 </html>
